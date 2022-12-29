0c3f5ffefce0aba2a1371466e81e36988b97d4b0
Onix25 <rishabhlakhwani06@gmail.com>
2022-12-29 19:26:34 +0530

    Added the about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :articles
 
   # root "application#hello"
   root "pages#home"
+# first is the url second is the pages controller and in the pages controller we are specifying the function
+  get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
