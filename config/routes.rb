4762b3305bc1bfac177df04fff6e1632242a6c0e
Onix25 <rishabhlakhwani06@gmail.com>
2022-12-31 09:03:09 +0530

    The recent changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # root "application#hello"
   root "pages#home"
 # first is the url second is the pages controller and in the pages controller we are specifying the function
-  get 'about', to: 'pages#about'
+  get 'about' to: 'pages#about'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
